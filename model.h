89c703595deb47dcab08c019ed7105c91dd684c3
vincent <yanchenglu@mail.com>
2020-06-02 18:53:39 +0800

    first commit

---
+++ model.h
@@ -0,0 +1,14 @@
+
+#ifndef MYAPI_MODEL_H
+
+#define MYAPI_MODEL_H
+
+extern "C" {
+    #include "php.h"
+    #include "ext/standard/info.h"
+    #include "php_myapi.h"
+}
+
+MYAPI_STARTUP_FUNCTION(model);
+
+#endif // MYAPI_MODEL_H
